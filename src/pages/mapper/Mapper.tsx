b2de07212bb3347442ece79275096f7b68622582
bavarianrhino <rjriesenberger@gmail.com>
2019-12-09 15:50:07 -0500

    added mapper page with previous code

--- src/pages/mapper/Mapper.tsx
+++ src/pages/mapper/Mapper.tsx
@@ -1,71 +1,81 @@
-import {
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonContent,
-  IonHeader,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar
-} from '@ionic/react';
-import { book, build, colorFill, grid } from 'ionicons/icons';
 import React from 'react';
-import './Tab1.css';
+import Mapp from './Mapp';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonPage, withIonLifeCycle } from '@ionic/react'; //IonContent,
+import PropTypes from 'prop-types'
+import { appConfig } from '../../utils/constants'; //appConfig
+import { UserSession, Person } from 'blockstack'
+// import { Location } from '../models/Location';
+// import { connect } from '../data/connect';
+// import * as selectors from '../data/selectors';
+// import './MapView.scss';
 
-const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab One</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonCard className="welcome-card">
-          <img src="/assets/shapes.svg" alt="" />
-          <IonCardHeader>
-            <IonCardSubtitle>Get Started</IonCardSubtitle>
-            <IonCardTitle>Welcome to Ionic</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <p>
-              Now that your app has been created, you'll want to start building out features and
-              components. Check out some of the resources below for next steps.
-            </p>
-          </IonCardContent>
-        </IonCard>
+// interface OwnProps { }
+// interface StateProps {
+//   locations: Location[];
+//   mapCenter: Location;
+// }
+// interface DispatchProps { }
+// interface MapViewProps extends OwnProps, StateProps, DispatchProps { };
+// const MappView: React.FC<MapViewProps> = ({ locations, mapCenter }) => {
+// const MappView: React.FC = () => {
 
-        <IonList lines="none">
-          <IonListHeader>
-            <IonLabel>Resources</IonLabel>
-          </IonListHeader>
-          <IonItem href="https://ionicframework.com/docs/" target="_blank">
-            <IonIcon slot="start" color="medium" icon={book} />
-            <IonLabel>Ionic Documentation</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/building/scaffolding" target="_blank">
-            <IonIcon slot="start" color="medium" icon={build} />
-            <IonLabel>Scaffold Out Your App</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/layout/structure" target="_blank">
-            <IonIcon slot="start" color="medium" icon={grid} />
-            <IonLabel>Change Your App Layout</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/theming/basics" target="_blank">
-            <IonIcon slot="start" color="medium" icon={colorFill} />
-            <IonLabel>Theme Your App</IonLabel>
-          </IonItem>
-        </IonList>
-      </IonContent>
-    </IonPage>
-  );
-};
+class MappView extends React.Component {
 
-export default Tab1;
+    state = {
+        userSession: this.props
+        // person: null,
+        // username: null
+    }
+
+    // static propTypes = {
+    //     userSession: PropTypes.object.isRequired,
+    //     history: PropTypes.object.isRequired
+    //   }
+
+    ionViewWillEnter() {
+        this.getUserSession()
+        
+    }
+    
+    getUserSession = () => {
+        // const userSession = new UserSession({ appConfig })
+        const userSession = this.props
+        // const username = this.props.userSession.loadUserData()
+        console.log(userSession)
+        console.log(this.state.userSession)
+        // const { userSession } = this.props
+        // console.log(userSesssion)
+        // this.setState({
+        //     ...this.state,
+        //     userSession: userSession,
+        //     // person: new Person(userSession.loadUserData().profile),
+        //     username: userSession.loadUserData().username
+        // })
+    }
+    render() {
+        const { userSession } = this.state
+        console.log(userSession)
+        const session = this.props
+        console.log(session)
+        console.log(sessionStorage)
+        return (
+            <IonPage id="map-view">
+                <IonHeader>
+                    <IonToolbar>
+                        <IonButtons slot="start">
+                            <IonMenuButton></IonMenuButton>
+                        </IonButtons>
+                        <IonTitle>Map</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+
+                {/* <IonContent class="map-page"> */}
+                    {/* <Map locations={locations} mapCenter={mapCenter} /> */}
+                    <Mapp userSession={userSession} />
+                {/* </IonContent> */}
+            </IonPage>
+        )
+    };
+}
+           
+export default withIonLifeCycle(MappView);

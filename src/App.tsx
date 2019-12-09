431b90c14cc2fac84f1082baf728e13277a04b4e
bavarianrhino <rjriesenberger@gmail.com>
2019-12-09 15:16:04 -0500

    added App router structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,22 @@
+//--------- Ionic & React ---------//
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs
-} from '@ionic/react';
+import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { apps, flash, send } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
-import Details from './pages/Details';
 
-/* Core CSS required for Ionic components to work properly */
+//--------- Pages ---------//
+import Login from './pages/login/Login'
+import Leaderboard from './pages/leaderboard/Leaderboard'
+import Details from './pages/leaderboard/Details'
+import Mapper from './pages/mapper/Mapper'
+import Logout from './pages/mogout/Logout'
+
+//--------- Core & Basic CSS ---------//
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
@@ -32,7 +24,7 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/* Theme variables */
+//--------- Theme Variables ---------//
 import './theme/variables.css';
 
 const App: React.FC = () => (
@@ -40,24 +32,25 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab2/details" component={Details} />
-          <Route path="/tab3" component={Tab3} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+          <Route path="/login" component={Login} exact={true} />
+          <Route path="/leaderboard" component={Leaderboard} exact={true} />
+          <Route path="/leaderboard/details" component={Details} />
+          <Route path="/mapper" component={Mapper} exact={true} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" render={() => <Redirect to="/login" />} exact={true} />
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
+          <IonTabButton tab="leaderboard" href="/leaderboard">
             <IonIcon icon={flash} />
-            <IonLabel>Tab One</IonLabel>
+            <IonLabel>Leaderboard</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
+          <IonTabButton tab="mapper" href="/mapper">
             <IonIcon icon={apps} />
-            <IonLabel>Tab Two</IonLabel>
+            <IonLabel>Map</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={send} />
-            <IonLabel>Tab Three</IonLabel>
+            <IonLabel>Logout</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
